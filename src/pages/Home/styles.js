ad288176570c9b41526a84dd2d30ebc39d59af20
Abner Willclefy <abner.monteiro.ext@atlasgov.com>
2022-04-08 16:37:59 -0300

    refactor(homepage): moved ContactsList jsx to Homepage

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
-export const Container = styled.header`
+export const Container = styled.div`
   margin-top: 32px;
+`;
+
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+    padding: 0 16px;
+
+    &::placeholder {
+      color: #bcbcbc;
+    }
+  }
 `;
 
 export const Header = styled.header`
@@ -9,6 +27,7 @@ export const Header = styled.header`
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  margin-top: 32px;
 
   strong {
     font-size: 24px;

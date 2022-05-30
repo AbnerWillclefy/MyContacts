4fc579b0634ed482dddb9bdd98484e20b0fdb9bb
Abner Willclefy <abner.monteiro.ext@atlasgov.com>
2022-05-30 11:39:04 -0300

    feat(loader): created react portals to use loader and modal components

---
+++ src/components/Loader/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable comma-dangle */
+
+import ReactDOM from 'react-dom';
+
+import { Overlay } from './styles';
+
+export default function Loader() {
+  return ReactDOM.createPortal(
+    <Overlay>
+      <div className="loader" />
+    </Overlay>,
+    document.getElementById('loader-root')
+  );
+}

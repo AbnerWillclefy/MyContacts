9d4d33f43b9cf527300b8cf7668692d2dfbebcb8
Abner Willclefy <abner.monteiro.ext@atlasgov.com>
2022-06-14 16:09:31 -0300

    feat(httpclient): added makeRequest method to HttpClient

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,11 +1,27 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import ContactsService from '../../services/ContactsService';
 
 export default function NewContact() {
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      await ContactsService.createContact(contact);
+    } catch {
+      alert('Ocorreu um erro ao cadastrar um contato');
+    }
+  }
+
   return (
     <>
       <PageHeader title="Novo contato" />
-      <ContactForm buttonLabel="Cadastrar" />
+      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
     </>
   );
 }

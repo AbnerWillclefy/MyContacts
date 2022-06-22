063ac68836ac9d7d7cc3c881d552589b1e020f6d
AbnerWillclefy <abnerwill.monteiro@gmail.com>
2022-06-21 22:55:42 -0300

    feat(eventmanager): added custom eventmanager to handle toasts

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
 import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function NewContact() {
   async function handleSubmit(formData) {
@@ -13,8 +14,16 @@ export default function NewContact() {
       };
 
       await ContactsService.createContact(contact);
+
+      toast({
+        type: 'success',
+        text: 'Contato cadastrado com sucesso!',
+      });
     } catch {
-      alert('Ocorreu um erro ao cadastrar um contato');
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao cadastrar o contato!',
+      });
     }
   }
 

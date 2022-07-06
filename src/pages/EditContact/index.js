b46d8914302b7e17665da810c3166f89a7726d94
AbnerWillclefy <abnerwill.monteiro@gmail.com>
2022-07-06 16:29:34 -0300

    feat(editcontact): added edit contact logic

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,11 +1,78 @@
+import { useEffect, useRef, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
 import ContactForm from '../../components/ContactForm';
 import PageHeader from '../../components/PageHeader';
+import Loader from '../../components/Loader';
+
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [contactName, setContactName] = useState('');
+  const contactFormRef = useRef(null);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contact = await ContactsService.getContactById(id);
+
+        contactFormRef.current.setFieldsValue(contact);
+        setIsLoading(false);
+        setContactName(contact.name);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  async function handleSubmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
+
+      const { name } = await ContactsService.updateContact(id, contact);
+
+      setContactName(name);
+
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
+  }
+
   return (
     <>
-      <PageHeader title="Editar Abner Willclefy" />
-      <ContactForm buttonLabel="Salvar alterações" />
+      <Loader isLoading={isLoading} />
+
+      <PageHeader title={isLoading ? 'Carregando...' : `Editar ${contactName}`} />
+
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }

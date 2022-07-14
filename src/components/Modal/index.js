8ec50fb00bb553a8e12cda48304b3553a43b5c39
AbnerWillclefy <abnerwill.monteiro@gmail.com>
2022-07-13 22:35:25 -0300

    feat(home): added delete contact operation

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -7,19 +7,46 @@ import { Container, Overlay, Footer } from './styles';
 
 import Button from '../Button';
 
-export default function Modal({ danger }) {
+export default function Modal({
+  danger,
+  title,
+  children,
+  confirmLabel,
+  cancelLabel,
+  onConfirm,
+  onCancel,
+  isOpen,
+  isLoading,
+}) {
+  if (!isOpen) {
+    return null;
+  }
+
   return ReactDOM.createPortal(
     <Overlay>
       <Container danger={danger}>
-        <h1>Título do modal</h1>
-        <p>Corpo do modal</p>
+        <h1>{title}</h1>
+
+        <div className="modal-body">
+          {children}
+        </div>
 
         <Footer>
-          <button type="button" className="cancel-button">
-            Cancelar
+          <button
+            type="button"
+            className="cancel-button"
+            onClick={onCancel}
+            disabled={isLoading}
+          >
+            {cancelLabel}
           </button>
-          <Button type="button" danger={danger}>
-            Deletar
+          <Button
+            type="button"
+            danger={danger}
+            onClick={onConfirm}
+            isLoading={isLoading}
+          >
+            {confirmLabel}
           </Button>
         </Footer>
       </Container>
@@ -30,8 +57,19 @@ export default function Modal({ danger }) {
 
 Modal.propTypes = {
   danger: PropTypes.bool,
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  confirmLabel: PropTypes.string,
+  cancelLabel: PropTypes.string,
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool,
 };
 
 Modal.defaultProps = {
   danger: false,
+  confirmLabel: 'Confirmar',
+  cancelLabel: 'Cancelar',
+  isLoading: true,
 };
--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -13,7 +13,7 @@ export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.6);
   backdrop-filter: blur(5px);
 
-  position: absolute;
+  position: fixed;
   left: 0;
   top: 0;
 
@@ -30,14 +30,14 @@ export const Container = styled.div`
   padding: 24px;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
 
-  h1 {
+  & > h1 {
     font-size: 22px;
     color: ${({ theme, danger }) =>
       danger ? theme.colors.danger.main : theme.colors.gray[900]};
   }
 
-  p {
-    margin-top: 8px;
+  .modal-body {
+    margin-top: 32px;
   }
 `;
 
@@ -52,7 +52,11 @@ export const Footer = styled.footer`
     background: transparent;
     border: none;
     font-size: 16px;
-    margin-right: 8px;
+    margin-right: 24px;
     color: ${({ theme }) => theme.colors.gray[200]};
+
+    &[disabled] {
+      cursor: default;
+    }
   }
 `;
